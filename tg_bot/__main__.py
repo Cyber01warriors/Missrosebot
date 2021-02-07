d792b53d638501196b450be6c7d0cb8000fc8ca9
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-02-07 23:11:19 +0400

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,13 +18,13 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hey there! My name is *{}*, I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
+Salam bratva mənim adım *{}*, I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 
 Join my [news channel](https://t.me/ProIndians) to get information on all the latest updates.
 """
 
 HELP_STRINGS = """
-Hey! My name is *{}*. I am a group management bot, here to help you get around and keep the order in your groups!
+Aəə mənim adım *{}*. I am a group management bot, here to help you get around and keep the order in your groups!
 I have lots of handy features, such as flood control, a warning system, a note keeping system, and even predetermined replies on certain keywords.
 
 *Helpful commands*:
